88605decbc294245268de19193f162f529878bcd
Taras <taras.holotiuk@gmail.com>
2024-11-27 15:46:15 +0200

    fix bugs

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -41,25 +41,21 @@ const ContactForm = ({ packageName, onClose }) => {
 		<div className="contact-form-container">
 			<div>
 				<h2>Форма обратной связи</h2>
-				<button className="close-button" onClick={onClose}>
-					&times;
-				</button>
+				{onClose && (
+					<button className="close-button" onClick={onClose}>
+						&times;
+					</button>
+				)}
 			</div>
 			<form id="contact-form" onSubmit={handleSubmit}>
 				<p>{packageName}</p>
 				<br />
 				<label htmlFor="name">Имя:</label>
 				<input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-				{/* <br /> */}
-
 				<label htmlFor="email">Электронная почта:</label>
 				<input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-				{/* <br /> */}
-
 				<label htmlFor="message">Сообщение:</label>
 				<textarea id="message" name="message" value={formData.message} onChange={handleChange} required></textarea>
-				{/* <br /> */}
-
 				<button className="button-send" type="submit">
 					Отправить
 				</button>
